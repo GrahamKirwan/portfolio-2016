b38b6b2b10f84aee0dcb823c03bb8a4b5bf1577e
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2021-01-27 12:59:22 -0500

    update

---
+++ stire-photobook/mobile/js/info.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var base_url="../6693B22FFA1C55E8732444E61FB5AEEA/";  
+var  number_of_pages=22;
+var needle=window.location.href.search("/mobile/");
+var mobile_url=(window.location.href.substr(0,needle))+"/mobile/";
+var res_url=(window.location.href).substr(0,needle)+"/"+base_url.substr(3)+"res/";
+var mobile_url=""; 
+var viewer_url=""; 
+function check_settings() {
+		
+	 
+}

0f9ec43e9f6a98f6d78c4477c6f459aa2d7a9367
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2021-02-02 09:50:51 -0500

    update

---
+++ photobook-update/mobile/js/info.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var base_url="../EEA55548BE8B59C33FC16334B1E11BE0/";  
+var  number_of_pages=26;
+var needle=window.location.href.search("/mobile/");
+var mobile_url=(window.location.href.substr(0,needle))+"/mobile/";
+var res_url=(window.location.href).substr(0,needle)+"/"+base_url.substr(3)+"res/";
+var mobile_url=""; 
+var viewer_url=""; 
+function check_settings() {
+		
+	 
+}
